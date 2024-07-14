e31a4c0499fd2c7899782f2b9f2e47d4920cf4da
PavanTuruk <153252840+PavanTuruk@users.noreply.github.com>
2024-07-14 15:27:43 +0530

    change resume

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
             <h1 className='text-violate '>Pavan Turuk</h1>
             <p className=' float-end'>React Developer</p>
         </div>
-            <button className='button px-4'><a href="/Pavan_ReactDeveloper_Resume.pdf" download className=' text-decoration-none text-light '>resume</a></button>
+            <button className='button px-4'><a href="/Pavan_ReactDeveloper_Resume1.pdf" download className=' text-decoration-none text-light '>resume</a></button>
       
     </div>
   )
